b6fc0d76953440b7b33974dcebe8a6af0575d143
Alastair Malkin <malkin@adobe.com>
2017-08-04 14:56:51 +0100

    Small change

--- alexa-personal/index.js
+++ alexa-personal/index.js
@@ -233,6 +233,17 @@ var startMalkinsHandlers = Alexa.CreateStateHandler(states.START_MODE, {
 //    END of Intent Handlers {} ========================================================================================
 // 3. Helper Function  =================================================================================================
 
+function supportsDisplay() {
+    var hasDisplay =
+        this.event.context &&
+        this.event.context.System &&
+        this.event.context.System.device &&
+        this.event.context.System.device.supportedInterfaces &&
+        this.event.context.System.device.supportedInterfaces.Display
+
+    return hasDisplay;
+}
+
 function birthdayCalculation(birthday, format) {
     console.log("in birthdayCalculation");
     var birthdaydate,nextbirthdaydate;
